a572d0109b5d715292cb1106a1bc01580f8e3cf3
Maxim Petischak <petrischak.maxim@gmail.com>
2020-03-23 21:30:03 +0200

    feat: create function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
+const isArray = array => !array || !array.length;
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.min = function min(array) {
+    return isArray(array) ? 0 : Math.min(...array);
+};
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    return isArray(array) ? 0 : Math.max(...array);
+};
 
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    return isArray(array)
+        ? 0
+        : array.reduce((total, current) => total + current, 0) / array.length;
+};
